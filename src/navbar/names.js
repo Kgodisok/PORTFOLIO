5466951dc0ddc24bb54b1cb88d403b70a6f01c59
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-13 12:32:00 +0200

    Add styles

--- src/navbar/names.js
+++ src/navbar/names.js
@@ -13,19 +13,22 @@ const imageStyles = {
     margin: '20px auto'
 };
 
+const footerStyles = {
+    backgroundColor: '#f1f1f1',
+    padding: '10px',
+    textAlign: 'center',
+    marginTop: '20px'
+};
 const NavBar = () => {
     return (
         <>
-        <h1 style={styles}>Hello there, Matsepe Kgodiso</h1>
+        <h1 style={styles}>Hello there,I am Matsepe Kgodiso</h1>
+        <h2>Aspiring Software Developer</h2>
         <p>Welcome to my portfolio</p>
         <img src='/images/image.png' alt='Matsepe Kgodiso' style={imageStyles}/>
         </>
     )
 }
-
-
-
-
 
 export default NavBar;
 
