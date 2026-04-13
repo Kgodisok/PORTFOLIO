f2743b36319588913c5d9c4e68ba5f609227c666
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-13 12:28:04 +0200

    Fix Typing Error

--- src/navbar/footer.js
+++ src/navbar/footer.js
@@ -4,9 +4,10 @@ const Footer = () => {
             <p>&copy; 2026 Matsepe Kgodiso portfolio. All rights reserved.</p>
             <p>Contact: matsepekgodiso474@gmail .com</p>
             <p>WhatsApp: +27 79 633 8442</p>
-            <p>LinkedIn: https://linkedin.com/in/matsepek-godiso</p>
+            <p>LinkedIn: https://linkedin.com/in/matsepe-kgodiso</p>
         </footer>
     );
 }
 
 export default Footer;
+
