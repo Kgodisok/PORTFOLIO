a4be8994c3c620e81d3360a57e9154ddd080f16b
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-13 12:25:14 +0200

    Add Foooter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import NavBar from './navbar/names';
+import Footer from './navbar/footer';
+  
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
 
 
 
---
+++ src/navbar/footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2026 Matsepe Kgodiso portfolio. All rights reserved.</p>
+            <p>Contact: matsepekgodiso474@gmail .com</p>
+            <p>WhatsApp: +27 79 633 8442</p>
+            <p>LinkedIn: https://linkedin.com/in/matsepek-godiso</p>
+        </footer>
+    );
+}
+
+export default Footer;
