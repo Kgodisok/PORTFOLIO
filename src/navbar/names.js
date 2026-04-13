d02a88414f3545da2bde8c00bfd3835e3ff05bb4
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-13 12:53:38 +0200

    Fix error

--- src/navbar/names.js
+++ src/navbar/names.js
@@ -24,6 +24,7 @@ const NavBar = () => {
         <>
         <h1 style={styles}>Hello there,I am Matsepe Kgodiso</h1>
         <h2>Aspiring Software Developer</h2>
+        <p>I'm a passionate and dedicated software developer with a strong desire to create innovative solutions. With a solid foundation in programming languages such as JavaScript, Python, and Java, I have experience in developing web applications, mobile apps, and software solutions. I am constantly learning and staying up-to-date with the latest technologies to enhance my skills and deliver high-quality work. I am eager to contribute my knowledge and creativity to projects that make a positive impact.</p>  
         <p>Welcome to my portfolio</p>
         <img src='/images/image.png' alt='Matsepe Kgodiso' style={imageStyles}/>
         </>
